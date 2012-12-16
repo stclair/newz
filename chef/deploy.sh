3681083703f433130c506ac3100020b0d06ec35d
Wes St. Clair <wes.stclair@theimtgroup.com>
2012-12-16 16:56:52 -0600

    deploy tweaks

--- chef/deploy.sh
+++ chef/deploy.sh
@@ -8,11 +8,15 @@ host="${1}"
 # we remove (-R) the old host key from known_hosts
 ssh-keygen -R "${host#*@}" 2> /dev/null
 
-rsync -r ../../newz $host:/home/ubuntu/www/
+rsync -r ../project $host:/home/ubuntu/www
 
 tar cj . | ssh -o 'StrictHostKeyChecking no' "$host" '
 sudo rm -rf ~/chef &&
 mkdir ~/chef &&
 cd ~/chef &&
 tar xj &&
-sudo bash install.sh'
+sudo bash install.sh
+
+rm -r /home/ubuntu/www/madisonian
+mv /home/ubuntu/www/project /home/ubuntu/www/madisonian
+'
